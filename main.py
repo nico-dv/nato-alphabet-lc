48c37aee8edb7f6ea2a9e332fb41d8767b9aa330
nico-dv <vv.nicov@gmail.com>
2022-06-24 22:01:49 +0100

    finished script

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
+import pandas as pd
 
-#TODO 1. Create a dictionary in this format:
-{"A": "Alfa", "B": "Bravo"}
 
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+with open("nato_phonetic_alphabet.csv") as npa_file:
+    npa = pd.read_csv(npa_file)
+
+
+npa_dictionary = {}
+pa_list = []
+
+for(index, row) in npa.iterrows():
+    npa_dictionary.update({row.letter: row.code})
+
+content = input("Insert a word:\n")
+
+content_list = [letter for letter in content.upper()]
+
+for element in content_list:
+    if element in npa_dictionary:
+        pa_list.append(npa_dictionary[element])
+
+
+
+print(content)
+print(pa_list)
